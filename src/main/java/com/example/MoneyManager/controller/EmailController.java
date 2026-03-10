0062ad320357d9d904265fee6f03a583cae2a505
skind <sahilkhurakte123@gmail.com>
2026-03-11 01:19:42 +0530

    Implemented Email button api call

---
+++ src/main/java/com/example/MoneyManager/controller/EmailController.java
@@ -0,0 +1,44 @@
+package com.example.MoneyManager.controller;
+
+import com.example.MoneyManager.model.Profile;
+import com.example.MoneyManager.service.*;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final ExcelService excelService;
+    private final IncomeService incomeService;
+    private final ExpenseService expenseService;
+    private final ActivationEmailService emailService;
+    private final ProfileService profileService;
+
+    @GetMapping("/income-excel")
+    public ResponseEntity<Void> emailIncomeExcelDetails() throws IOException, MessagingException {
+        Profile profile = profileService.getCurrentProfile();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        excelService.writeIncomesToExcel(baos,incomeService.getCurrentMonthIncomesForCurrentUser());
+        emailService.sendEmailWithAttatchment(profile.getEmail(), "Your Income Excel Report", "PFA your income report for current month", baos.toByteArray(), "income_report.xlsx");
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/expense-excel")
+    public ResponseEntity<Void> emailExpenseExcelDetails() throws IOException, MessagingException {
+        Profile profile = profileService.getCurrentProfile();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        excelService.writeExpenseToExcel(baos,expenseService.getCurrentMonthExpensesForCurrentUser());
+        emailService.sendEmailWithAttatchment(profile.getEmail(), "Your Expense Excel Report", "PFA your expense report for current month", baos.toByteArray(), "income_report.xlsx");
+        return ResponseEntity.ok(null);
+    }
+
+}
--- src/main/java/com/example/MoneyManager/service/ActivationEmailService.java
+++ src/main/java/com/example/MoneyManager/service/ActivationEmailService.java
@@ -1,8 +1,14 @@
 package com.example.MoneyManager.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.util.ByteArrayDataSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -35,4 +41,14 @@ public class ActivationEmailService {
 
     }
 
+    public void sendEmailWithAttatchment(String to, String subject, String body, byte[] attachment, String filename) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body);
+        helper.addAttachment(filename, new ByteArrayResource(attachment));
+        mailSender.send(message);
+    }
 }
